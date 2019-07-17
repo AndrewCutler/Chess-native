1f6e81f13293b186359d63b4945c02088afc8874
Andrew Cutler <spyroninja@gmail.com>
2019-07-17 14:43:17 -0400

    started draggables

--- components/Piece.js
+++ components/Piece.js
@@ -1,49 +1,80 @@
 import React from 'react'
 import { pieces } from '../pieces'
-import { Image } from 'react-native'
+import { Image, StyleSheet, PanResponder, Animated } from 'react-native'
 
-const Piece = ({ pieceName }) => {
-  // let imgSource = '../assets/pieces/wikipedia/bK.png'
-  let imgSource
-  switch (pieceName) {
-    case 'bB':
-      imgSource = pieces.bB.uri
-      break
-    case 'bP':
-      imgSource = pieces.bP.uri
-      break
-    case 'bK':
-      imgSource = pieces.bK.uri
-      break
-    case 'bQ':
-      imgSource = pieces.bQ.uri
-      break
-    case 'bN':
-      imgSource = pieces.bN.uri
-      break
-    case 'bR':
-      imgSource = pieces.bR.uri
-      break
-    case 'wB':
-      imgSource = pieces.wB.uri
-      break
-    case 'wP':
-      imgSource = pieces.wP.uri
-      break
-    case 'wK':
-      imgSource = pieces.wK.uri
-      break
-    case 'wQ':
-      imgSource = pieces.wQ.uri
-      break
-    case 'wN':
-      imgSource = pieces.wN.uri
-      break
-    case 'wR':
-      imgSource = pieces.wR.uri
-      break
+export default class Piece extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pan: new Animated.ValueXY()
+    }
+
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([
+        null,
+        {
+          dx: this.state.pan.x,
+          dy: this.state.pan.y
+        }
+      ]),
+      onPanResponderRelease: (e, gesture) => {}
+    })
   }
-  return <Image source={imgSource} style={{ width: 30, height: 30 }} />
-}
+  render() {
+    let imgSource
+    switch (this.props.pieceName) {
+      case 'bB':
+        imgSource = pieces.bB.uri
+        break
+      case 'bP':
+        imgSource = pieces.bP.uri
+        break
+      case 'bK':
+        imgSource = pieces.bK.uri
+        break
+      case 'bQ':
+        imgSource = pieces.bQ.uri
+        break
+      case 'bN':
+        imgSource = pieces.bN.uri
+        break
+      case 'bR':
+        imgSource = pieces.bR.uri
+        break
+      case 'wB':
+        imgSource = pieces.wB.uri
+        break
+      case 'wP':
+        imgSource = pieces.wP.uri
+        break
+      case 'wK':
+        imgSource = pieces.wK.uri
+        break
+      case 'wQ':
+        imgSource = pieces.wQ.uri
+        break
+      case 'wN':
+        imgSource = pieces.wN.uri
+        break
+      case 'wR':
+        imgSource = pieces.wR.uri
+        break
+    }
 
-export default Piece
+    const styles = StyleSheet.create({
+      piece: {
+        width: 35,
+        height: 35
+      }
+    })
+
+    return (
+      <Animated.Image
+        source={imgSource}
+        {...this.panResponder.panHandlers}
+        style={[this.state.pan.getLayout(), styles.piece]}
+      />
+    )
+  }
+}
--- components/Square.js
+++ components/Square.js
@@ -1,23 +1,43 @@
 import React from 'react'
 import { View, StyleSheet } from 'react-native'
 
-const Square = ({ matrixID, squareID }) => {
-  const styles = StyleSheet.create({
-    light: {
-      backgroundColor: '#fff',
-      width: 45,
-      height: 45
-    },
-    dark: {
-      backgroundColor: '#8833dd',
-      width: 45,
-      height: 45
+export default class Square extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      squareCoords: null
     }
-  })
-  const squareColor =
-    matrixID.rank % 2 === matrixID.file % 2 ? styles.light : styles.dark
+  }
+  findSquareCoords = e => {
+    this.setState({
+      squareCoords: e.nativeEvent.layout
+    })
+  }
+  render() {
+    // const Square = ({ matrixID, squareID }) => {
+    const styles = StyleSheet.create({
+      light: {
+        backgroundColor: '#fff',
+        width: 45,
+        height: 45
+      },
+      dark: {
+        backgroundColor: '#8833dd',
+        width: 45,
+        height: 45
+      }
+    })
+    const squareColor =
+      this.props.matrixID.rank % 2 === this.props.matrixID.file % 2
+        ? styles.light
+        : styles.dark
 
-  return <View style={squareColor} onTouchStart={() => console.log(squareID)} />
+    return (
+      <View
+        style={squareColor}
+        onLayout={this.findSquareCoords}
+        onTouchStart={() => console.log(squareID)}
+      />
+    )
+  }
 }
-
-export default Square
